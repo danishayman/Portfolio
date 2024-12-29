c1ecc1679e23673a1fb9f77d147ecb8a64f597f6
ayman <danishaiman3b@gmail.com>
2024-12-29 19:00:47 +0800

    refactor Projects and Skills components for improved structure and add new project entries

--- src/component/Projects/Projects.jsx
+++ src/component/Projects/Projects.jsx
@@ -1,37 +1,47 @@
-import styles from "./Projects.module.css";
-import law from "../../assets/law.png";
-import gasket from "../../assets/3D gasket.png";
-import ProjectCard from "../../common/ProjectCard";
-
+import styles from './Projects.module.css';
+import gasket from '../../assets/gasket.png';
+import law from '../../assets/law.png';
+import drink from '../../assets/drink.png';
+import compiler from '../../assets/compiler.png';
+import cervical from '../../assets/cervical.png';
+import ProjectCard from '../../common/ProjectCard';
 
 function Projects() {
   return (
     <section id="projects" className={styles.container}>
-      <h1 className="sectionTitle"> Projects </h1>
+      <h1 className="sectionTitle">Projects</h1>
       <div className={styles.projectContainer}>
+        <ProjectCard
+          src={gasket}
+          link="https://danishayman.github.io/3D-Sierpinski-Gasket/"
+          h3="3D Sierpinski Gasket"
+          p="Interactive WebGL Application"
+        />
+        <ProjectCard
+          src={law}
+          link="https://github.com/danishayman/Law-Firm-Management"
+          h3="Law Firm Management"
+          p="JavaFx Application"
+        />
+        <ProjectCard
+          src={drink}
+          link="https://github.com/danishayman/drink-your-water-discord-bot"
+          h3="Drink Your Water"
+          p="Discord Bot"
+        />
+        <ProjectCard
+          src={compiler}
+          link="https://github.com/danishayman/Simple-Compiler"
+          h3="MiniLang"
+          p="A Simple Compiler"
+        />
 
-        <ProjectCard 
-        src={law} link = "https://github.com/danishayman/Law-Firm-Management"
-        h3 = "Law Firm Management" 
-        p = "Law Firm Management App using JavaFX and Scene Builder"/>
-
-        <ProjectCard 
-        src={gasket} link = "https://danishayman.github.io/3D-Sierpinski-Gasket/"
-        h3 = "3D Sierpinski Gasket" 
-        p = "Interactive WebGL application that renders a 3D Sierpinski Gasket"/>
-
-    
-        <ProjectCard 
-        src={law} link = "https://github.com/danishayman/Law-Firm-Management"
-        h3 = "Law Firm Management" 
-        p = "Law Firm Management App using JavaFX"/>
-
-
-        <ProjectCard 
-        src={gasket} link = "https://danishayman.github.io/3D-Sierpinski-Gasket/"
-        h3 = "3D Sierpinski Gasket" 
-        p = "Interactive WebGL application that renders a 3D Sierpinski Gasket"/>
-
+        <ProjectCard
+          src={cervical}
+          link="https://github.com/danishayman/Cervical-Cancer-Predictive-Model"
+          h3="Cervical Cancer Prediction"
+          p="Machine Learning Model"
+        />
       </div>
     </section>
   );
--- src/component/Skills/Skills.jsx
+++ src/component/Skills/Skills.jsx
@@ -15,23 +15,23 @@ function Skills() {
         <SkillList src={checkMarkIcon} skill="HTML" />
         <SkillList src={checkMarkIcon} skill="CSS" />
         <SkillList src={checkMarkIcon} skill="JavaScript" />
-        <SkillList src={checkMarkIcon} skill="TypeScript" />
-        <SkillList src={checkMarkIcon} skill="Node" />
+        <SkillList src={checkMarkIcon} skill="React.js" />
+        <SkillList src={checkMarkIcon} skill="Node.js" />
       </div>
       <hr />
       <div className={styles.skillList}>
-        <SkillList src={checkMarkIcon} skill="React" />
-        <SkillList src={checkMarkIcon} skill="Angular" />
-        <SkillList src={checkMarkIcon} skill="Vue" />
-        <SkillList src={checkMarkIcon} skill="Tailwind CSS" />
+        <SkillList src={checkMarkIcon} skill="Python" />
+        <SkillList src={checkMarkIcon} skill="Java" />
+        <SkillList src={checkMarkIcon} skill="C++" />
+        <SkillList src={checkMarkIcon} skill="SQL" />
       </div>
       <hr />
       <div className={styles.skillList}>
-        <SkillList src={checkMarkIcon} skill="Redux" />
-        <SkillList src={checkMarkIcon} skill="Webpack" />
+        <SkillList src={checkMarkIcon} skill="Pandas" />
+        <SkillList src={checkMarkIcon} skill="NumPy" />
         <SkillList src={checkMarkIcon} skill="Git" />
-        <SkillList src={checkMarkIcon} skill="Jest" />
-        <SkillList src={checkMarkIcon} skill="Bootstrap" />
+        <SkillList src={checkMarkIcon} skill="TensorFlow" />
+        <SkillList src={checkMarkIcon} skill="Scikit-learn" />
       </div>
     </section>
   );
