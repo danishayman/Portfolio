a6719827de342a811ed7bc30876618ef5d11317e
ayman <danishaiman3b@gmail.com>
2024-12-29 17:19:35 +0800

    add Skills component and SkillList for skill display

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import Hero from "./component/Hero/Hero";
+import Skills from "./component/Skills/Skills";
 import Projects from "./component/Projects/Projects";
 
 function App() {
   return (
     <>
       <Hero />
+      <Skills />
       <Projects />
     </>
   );
