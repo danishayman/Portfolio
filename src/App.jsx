e3e4306acf9bf5df9b3c2e8bb0700d82a783cbc0
ayman <danishaiman3b@gmail.com>
2024-12-29 17:42:42 +0800

    add Footer component with styling and integrate into App

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Hero from "./component/Hero/Hero";
 import Skills from "./component/Skills/Skills";
 import Projects from "./component/Projects/Projects";
 import Contact from "./component/Contact/Contact";
+import Footer from "./component/Footer/Footer";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Skills />
       <Projects />
       <Contact />
+      <Footer />
     </>
   );
 }
---
+++ src/component/Footer/Footer.jsx
@@ -0,0 +1,16 @@
+import styles from './Footer.module.css';
+
+function Footer() {
+  return (
+    <section id="footer" className={styles.container}>
+        <p>
+            &copy; 2024 Danish Aiman.<br/>
+            All rights reserved.
+            
+        </p>
+
+    </section>
+  );
+}
+
+export default Footer;
