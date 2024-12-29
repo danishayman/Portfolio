1a14fd84a097f5160fc86f80a3d2d4590fb17d6d
ayman <danishaiman3b@gmail.com>
2024-12-29 17:38:47 +0800

    add Contact component with form for user inquiries

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import Hero from "./component/Hero/Hero";
 import Skills from "./component/Skills/Skills";
 import Projects from "./component/Projects/Projects";
+import Contact from "./component/Contact/Contact";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Hero />
       <Skills />
       <Projects />
+      <Contact />
     </>
   );
 }
