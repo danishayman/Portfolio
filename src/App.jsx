03601f845a663a1089d57d8173039eae925387f6
ayman <danishaiman3b@gmail.com>
2024-12-29 16:50:43 +0800

    initialize React project with Vite setup and basic components

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Hero from "./component/Hero/Hero";
+import Projects from "./component/Projects/Projects";
+
+function App() {
+  return (
+    <>
+      <Hero />
+      <Projects />
+    </>
+  );
+}
+
+export default App;
---
+++ src/component/Projects/Projects.jsx
@@ -0,0 +1,34 @@
+import styles from "./Projects.module.css";
+import law from "../../assets/law.png";
+import gasket from "../../assets/3D gasket.png";
+import ProjectCard from "../../common/ProjectCard";
+
+
+function Projects() {
+  return (
+    <section id="projects" className={styles.container}>
+      <h1 className="sectionTitle"> Projects </h1>
+      <div className={styles.projectContainer}>
+
+        <ProjectCard 
+        src={law} link = "https://github.com/danishayman/Law-Firm-Management"
+        h3 = "Law Firm Management" 
+        p = "Law Firm Management App using JavaFX and Scene Builder"/>
+
+        <ProjectCard 
+        src={gasket} link = "https://danishayman.github.io/3D-Sierpinski-Gasket/"
+        h3 = "3D Sierpinski Gasket" 
+        p = "Interactive WebGL application that renders a 3D Sierpinski Gasket"/>
+
+    
+        <ProjectCard 
+        src={law} link = "https://github.com/danishayman/Law-Firm-Management"
+        h3 = "Law Firm Management" 
+        p = "Law Firm Management App using JavaFX"/>
+
+      </div>
+    </section>
+  );
+}
+
+export default Projects;
